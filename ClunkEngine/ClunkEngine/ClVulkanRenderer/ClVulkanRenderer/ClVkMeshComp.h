dc5368e8dab0ba548202e97c2e83501fd16a7188
joervargas <joervargas3@gmail.com>
2023-05-13 13:16:05 +0900

    altered ClVkMeshComp

--- ClunkEngine/ClunkEngine/ClVulkanRenderer/ClVulkanRenderer/ClVkMeshComp.h
+++ ClunkEngine/ClunkEngine/ClVulkanRenderer/ClVulkanRenderer/ClVkMeshComp.h
@@ -15,28 +15,26 @@ namespace Clunk
     class ClVkMeshComp : public ClIMeshComp
     {
     public:
-
-        // TODO: Make VkPipelineLayout a member variable
         
-        virtual void Load() {};
-        virtual void LoadFromFile(String MeshPath, String TexturePath = "", Vec3 Color = Vec3(1.f, 0.6f, 0.f));
-        virtual void Unload();
+        // virtual void Load() {};
+        // virtual void LoadFromFile(String MeshPath, String TexturePath = "", Vec3 Color = Vec3(1.f, 0.6f, 0.f));
+        // virtual void Unload();
 
-        virtual void Render(
-            const VkCommandBuffer& DrawBuffer,
-            const VkPipelineLayout& PipelineLayout,
-            const VkBuffer& TransformUBO,
-            const Transform3d& MeshTransform
-        );
+        // virtual void Render(
+        //     const VkCommandBuffer& DrawBuffer,
+        //     const VkPipelineLayout& PipelineLayout,
+        //     const VkBuffer& TransformUBO,
+        //     const Transform3d& MeshTransform
+        // );
 
-        virtual void Destroy();
+        // virtual void Destroy();
 
     protected:
 
         ClVkArrayBuffer m_VertBuffer;
         ClVkArrayBuffer m_IndexBuffer;
-        ClVkTexture m_Texture;
-        Vec3 m_color;
+        // ClVkTexture m_Texture;
+        // Vec3 m_color;
 
     private:
 
